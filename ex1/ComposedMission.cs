2fd6a9433fdb16e9f3cc06e0bc00436abd99854d
Roy Hevrony <roy.hevrony@live.biu.ac.il>
2019-03-14 23:45:23 +0200

    Composed Mission implementation

--- ex1/ComposedMission.cs
+++ ex1/ComposedMission.cs
@@ -8,6 +8,48 @@ namespace Excercise_1
 {
     public class ComposedMission : IMission
     {
-      
+        private string name;
+        private string type;
+        private List<MathFuncionPointer> mathFuncPtrList;
+
+        public event EventHandler<double> OnCalculate;
+
+        public ComposedMission(string name)
+        {
+            this.name = name;
+            this.type = "Composed";
+            this.mathFuncPtrList = new List<MathFuncionPointer>();
+        }
+
+        public string Name {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public string Type {
+            get {
+                return this.type;
+            }
+        }
+
+        public ComposedMission Add (MathFuncionPointer ptr)
+        {
+            this.mathFuncPtrList.Add(ptr);
+            return this;
+        }
+
+        
+
+        public double Calculate(double value)
+        {
+            double result = value;
+            foreach (var funcPtr in this.mathFuncPtrList) {
+                result = funcPtr(result);
+            }
+            OnCalculate?.Invoke(this, result);
+            return result;
+        }
     }
 }
