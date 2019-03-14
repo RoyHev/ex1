8d50f4fbd074b379da8ee6479ab652de71d0142d
Roy Hevrony <roy.hevrony@live.biu.ac.il>
2019-03-14 18:52:48 +0200

    FunctionsContainer start

---
+++ ex1/MissionContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Excercise_1
+{
+    public delegate double MathFuncionPointer(double d);
+
+    public class FunctionsContainer {
+
+        private Dictionary<string, MathFuncionPointer> funcDictionary;
+
+        public FunctionsContainer() {
+
+            funcDictionary = new Dictionary<string, MathFuncionPointer>();
+        }
+
+        public MathFuncionPointer this[string key] {
+
+            get {
+                if (funcDictionary.ContainsKey(key)){
+                    return funcDictionary[key];
+                } else {
+                    funcDictionary[key] = val => val;
+                    return funcDictionary[key];
+                }
+            }
+
+            set {
+                if (value != null) {
+                    funcDictionary[key] = value;
+                }
+            }
+        }
+       
+    }
+}
